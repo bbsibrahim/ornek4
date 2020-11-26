36bd389f0941d3501ef03f4a8026aa6b5cfa31e9
bbsibrahim <bbsibrahim@gmail.com>
2020-11-26 22:29:58 +0000

    Sepete eklendi

--- src/test4.java
+++ src/test4.java
@@ -2,4 +2,9 @@ public class test4 {
     public static void main(String[] args) {
         System.out.println("1. islem");
     }
+    public static void yeniMethod()
+    {
+        System.out.println("Yeni ethodum hazir");
+        System.out.println("Sepete ekle");
+    }
 }
